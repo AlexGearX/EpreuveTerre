ccf20153f9ea987adb75104c8b73ec5691ef36fc
AlexGearX <alexgear34@gmail.com>
2022-06-28 12:18:07 +0200

    alphabet Ok

--- 1-alphabet.js
+++ 1-alphabet.js
@@ -1,1 +1,8 @@
-var readline = require("readline-sync");
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabetEnPhrase = "";
+
+for (let index = 0; index < alphabet.length; index++) {
+    alphabetEnPhrase += alphabet[index];
+    console.log(alphabet[index]);
+}
+console.log(alphabetEnPhrase + '\n');
