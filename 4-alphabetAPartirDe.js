bd4d07abe1e16db86ec0cb90b94782db1a9b80f2
AlexGearX <alexgear34@gmail.com>
2022-06-29 10:05:24 +0200

    alphabetAPartirDe Ok

--- 4-alphabetAPartirDe.js
+++ 4-alphabetAPartirDe.js
@@ -1,1 +1,18 @@
-var readline = require("readline-sync");
+var myArgs = process.argv.slice(2);
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabetAPartirDeArg = "";
+
+for (let index = 0; index < alphabet.length; index++) {
+    if (alphabet[index] == myArgs[0].toLowerCase()) {
+        for (let a = index; a < alphabet.length; a++) {
+            alphabetAPartirDeArg += alphabet[a];
+        }
+        break;
+    }
+}
+
+if (alphabetAPartirDeArg == "") {
+    console.log("l'arguments n'est pas une lettre")
+} else {
+    console.log(alphabetAPartirDeArg);
+}
