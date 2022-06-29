f1c5af529ac8a6d455818d3b1c360e5a11b34688
AlexGearX <alexgear34@gmail.com>
2022-06-29 18:14:56 +0200

    ex 12 OK

--- 12-24To12.js
+++ 12-24To12.js
@@ -1,1 +1,16 @@
-var readline = require("readline-sync");
+var myArgs = process.argv.slice(2);
+
+if (myArgs.length == 1) {
+    myArgs[0].split('');
+} else {
+    Error();
+}
+
+function Error() {
+    console.log("Erreur.");
+    process.exit(1);
+}
+
+function isNumeric(num) {
+    return !isNaN(num);
+}
